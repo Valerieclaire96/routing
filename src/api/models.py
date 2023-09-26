750f3d9cd518a7556147d1c130434a82fd9f1b0a
Valerieclaire96 <Valerieclaire96@gmail.com>
2023-09-26 01:25:51 +0000

    practice

--- src/api/models.py
+++ src/api/models.py
@@ -17,3 +17,29 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+
+class DontationInfo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    full_name = db.Column(db.String(120), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    address = db.Column(db.String(500), nullable=False)
+    phone_number = db.Column(db.Integer, nullable=False)
+
+    # establish the relationship between user and donations
+    # establish the relation between donator and payment proccessing
+    
+    # credit card
+    def __repr__(self):
+        return f'<User {self.email}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "full_name": self.first_name,
+            "email": self.email,
+            "address": self.address,
+            "phone_number": self.phone_number,
+            
+            # do not serialize the card credit, its a security breach
+        }
--- src/api/routes.py
+++ src/api/routes.py
@@ -2,7 +2,7 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from api.models import db, User, DontationInfo
 from api.utils import generate_sitemap, APIException
 
 api = Blueprint('api', __name__)
@@ -16,3 +16,40 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@api.route('/donation_info', methods=["POST"])
+def handle_adding_domain_info():
+      request_body = request.get_json()
+      full_name = request_body('full_name')
+      email = request_body('email')
+      address = request_body('address')
+      phone_number = request_body('phone_number')
+      
+    #   adding more code to scramble the credit card number 
+      donation_info = DontationInfo.query.filter_by(email=email)
+      if donation_info is not None:
+          return jsonify({'message': 'User already exists'}), 400
+      
+      new_donation_info = DontationInfo(full_name=full_name, email=email, address=address, phone_number=phone_number)
+      db.session.add(new_donation_info)
+      db.session.commit()
+      return jsonify(new_donation_info.serialize()), 201
+  
+@api.route('/donation_info', methods=["GET"])
+# jwt required maybe depending on the relationship between user and donator 
+def handle_get_all_domain_info():
+    donation_info = DontationInfo.query.all()
+    
+    if donation_info is not None:
+          return jsonify(donation_info), 200
+      
+    return jsonify({'message': 'User doesn`t exists'}), 400
+    
+# get each route
+@api.route('/dontain_info/<int:id>', methods=["GET"])
+# jwt required
+def handle_get_each_donation_info(id):
+    each_donation_info = DontationInfo.filter_by(id=id).first()
+    
+    return jsonify(each_donation_info.serialize())
+    
